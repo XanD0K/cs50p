79ba0940fe90dc6eebd872fc66b44914755e24fd
XanD0K <element_trading_47@icloud.com>
2025-08-14 09:20:25 -0300

    Submit pset1 'interpreter'

---
+++ pset1/interpreter/interpreter.py
@@ -0,0 +1,18 @@
+expression = input("Expression: ")
+
+x, y, z = expression.split(" ")
+
+x = int(x)
+z = int(z)
+
+if y == "+":
+    print(float(x + z))
+elif y == "-":
+    print(float(x - z))
+elif y == "*":
+    print(float(x * z))
+elif y == "/":
+    if z == 0:
+        print("Can't divide by zero!")
+    else:
+        print(float(x / z))
