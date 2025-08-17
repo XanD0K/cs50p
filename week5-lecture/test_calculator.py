f8b98dd6b3580c5d7d7af34dc9cacdb7e7dfc355
Alexandre Duarte Ferrari <element_trading_47@icloud.com>
2025-08-17 20:41:27 +0000

    Started week5 lecture

---
+++ week5-lecture/test_calculator.py
@@ -0,0 +1,16 @@
+# This code has the purpose of test calculator.py, specifically, its square() function
+
+from calculator import square
+
+def main():
+    test_square()
+
+
+def test_square():
+    if square(2) != 4:
+        print("2 square was not 4")
+    if square(3) != 9:
+        print("3 square was not 9")
+
+if __name__ == "__main__":
+    main()
