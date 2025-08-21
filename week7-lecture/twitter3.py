f75852fc0bec191d179fb0b7cd805fd0ca5d701c
XanD0K <element_trading_47@icloud.com>
2025-08-21 10:19:54 -0300

    Fixed typo

--- week7-lecture/twitter3.py
+++ week7-lecture/twitter3.py
@@ -5,4 +5,4 @@ url = input("URL: ").strip()
 # instead of [a-z0-9_], I could use '\w'
 # Removed the '$' sign at the end to allow more stuff after the username
 if matches := re.search(r"^(?:https?://)?(?:www.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
-    print(f"Username?", matches.group(1))
+    print(f"Username: ", matches.group(1))
